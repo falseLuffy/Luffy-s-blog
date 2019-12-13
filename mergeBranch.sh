fb22d49c4155abb9ef6ba7513bf4f232f1c0a0f6
falseLuffy <falseLuffy@qq.com>
2019-12-13 16:29:56 +0800

    本地多分支的合并

---
+++ mergeBranch.sh
@@ -0,0 +1,28 @@
+function mergeBranch () {
+    git checkout feature/provincial-procuratorate
+    git pull
+    git merge "$1"
+    git push
+    echo "合并$1分支到provincial-procuratorate"
+
+    for branch in demo developing
+    do
+        git checkout "$branch"
+        git pull 
+        git merge remotes/origin/feature/provincial-procuratorate
+        git push
+        echo "合并provincial-procuratorate分支到$branch"
+    done
+    git checkout "$1"  
+}
+
+cd /e/futureData/fd-vue-wpd
+branch="`git branch 2>/dev/null | grep "^\*" | sed -e "s/^\*\ //"`"
+echo "当前分支是$branch"
+if test $branch = 'feature/ruixu20191213' ;then 
+    mergeBranch $branch
+else 
+    echo '分支错了吧'
+fi
+
+cd -
