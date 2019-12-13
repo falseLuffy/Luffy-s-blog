232572db795e66ab7ed3d38b83fcf73682cb1175
falseLuffy <falseLuffy@qq.com>
2019-12-13 16:29:40 +0800

    开发环境得部署脚本

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+function executive () {
+    echo "将部署$1"
+        rm -rf `ls | grep -v "back"`
+        cp  $1 ./
+        unzip -o ./dist*.zip > /dev/null
+        mv ./dist/*  ./
+        mv ./dist*.zip ./back/
+        rm -rf ./dist
+    
+}
+
+cd /usr/local/nginx/html/fd
+# 参数是否为空
+if [ ! -n "$1" ] ;then
+    file=`find /data/package/web-page/ -mmin -100000 -type f | head -1`
+    if [ ! -n "$file" ] ;then
+        echo '未发现符合条件的文件'
+    else 
+        executive $file
+    fi
+# 参数指定的文件是否存在
+elif [ ! -f "/data/package/web-page/$1" ] ;then 
+    echo "$1 is not exist"
+else 
+    file="/data/package/web-page/$1"
+    executive $file
+fi
+cd -
